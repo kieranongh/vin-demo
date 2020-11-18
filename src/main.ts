306289afaef70c976f7e3ee9bf0501f9756f5eaf
Kieran Lomas <kieran.lomas@bom.gov.au>
2020-11-18 14:26:36 +0000

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
+  app.enableCors()
   const port = process.env.NODE_PORT || 43000
-  await app.listen(port);
+  await app.listen(port)
 }
 bootstrap();
