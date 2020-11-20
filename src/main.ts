7e3d21b7c033dad37906fb204200a0ff7a195963
Kieran Lomas <kieran.lomas@bom.gov.au>
2020-11-20 01:10:48 +0000

    Rename env variable for port

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
-  const port = process.env.NODE_PORT || 43000
+  const port = process.env.API_PORT || 43000
   await app.listen(port)
 }
 bootstrap();
